5b4e5e696c47739534b2642b122fff3501ddb9d8
codingconcepts <hello@robreid.io>
2023-12-04 21:00:02 +0000

    feat: Working example of a very simple first-cut

--- README.md
+++ README.md
@@ -1,2 +1,65 @@
 # lb
 A simple TCP load balancer
+
+### Run locally
+
+Start clusters and lb
+
+``` sh
+cockroach start-single-node \
+  --insecure \
+  --listen-addr=localhost:26001 \
+  --http-addr=localhost:8001 \
+  --store node1 \
+  --background
+
+cockroach start-single-node \
+  --insecure \
+  --listen-addr=localhost:26002 \
+  --http-addr=localhost:8002 \
+  --store node2 \
+  --background
+
+go run lb.go \
+  -server "localhost:26001" \
+  -server "localhost:26002" \
+  -port 26000
+```
+
+Run a command against one cluster
+
+``` sh
+cockroach sql \
+  --host localhost:26000 \
+  --insecure \
+  -e "CREATE TABLE a (id UUID PRIMARY KEY)"
+```
+
+Change lb to point to the other server
+
+Run a command against the other cluster
+
+``` sh
+cockroach sql \
+  --host localhost:26000 \
+  --insecure \
+  -e "CREATE TABLE b (id UUID PRIMARY KEY)"
+```
+
+Confirm that the tables have been created in the expected clusters
+
+``` sh
+cockroach sql \
+  --host localhost:26001 \
+  --insecure \
+  -e "SHOW TABLES"
+
+cockroach sql \
+  --host localhost:26002 \
+  --insecure \
+  -e "SHOW TABLES"
+```
+
+### Todos
+
+* Allow server to block requests (for things like cutovers).
---
+++ lb.go
@@ -0,0 +1,92 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net"
+	"strings"
+
+	"github.com/codingconcepts/lb/pkg/list"
+)
+
+var (
+	selectedServer string
+)
+
+func main() {
+	var sf stringFlags
+	flag.Var(&sf, "server", "a collection of servers to talk to")
+
+	port := flag.Int("port", 26257, "port number to listen on")
+	flag.Parse()
+
+	// Validate.
+	if len(sf) == 0 {
+		log.Fatalf("need at least 1 server")
+	}
+
+	selectionChanged := make(chan string, 1)
+
+	go handleServerChanged(selectionChanged)
+
+	go func() {
+		if err := list.RenderList(sf, selectionChanged); err != nil {
+			log.Fatalf("error rendering list: %v", err)
+		}
+	}()
+
+	proxyAddr := fmt.Sprintf("localhost:%d", *port)
+	listener, err := net.Listen("tcp", proxyAddr)
+	if err != nil {
+		log.Fatalf("error starting proxy server: %v", err)
+	}
+
+	for {
+		client, err := listener.Accept()
+		if err != nil {
+			fmt.Println("error accepting client connection:", err)
+			continue
+		}
+
+		go handleClient(client, selectedServer)
+	}
+}
+
+func handleServerChanged(selectedChanged chan string) {
+	for server := range selectedChanged {
+		selectedServer = server
+	}
+}
+
+func handleClient(client net.Conn, server string) {
+	defer client.Close()
+
+	nonHTTPServer, err := net.Dial("tcp", server)
+	if err != nil {
+		log.Printf("error connecting to server: %v", err)
+		return
+	}
+	defer nonHTTPServer.Close()
+
+	go io.Copy(nonHTTPServer, client)
+	io.Copy(client, nonHTTPServer)
+}
+
+type stringFlags []string
+
+func (sf *stringFlags) String() string {
+	sb := strings.Builder{}
+
+	for i, s := range *sf {
+		sb.WriteString(fmt.Sprintf("[%d] %s\n", i+1, s))
+	}
+
+	return sb.String()
+}
+
+func (sf *stringFlags) Set(value string) error {
+	*sf = append(*sf, value)
+	return nil
+}
